48f854f42644dc442c7dcddfc3053eb58bcb10ef
pacifi <pacifi.bnr@gmail.com>
2015-08-23 15:30:36 -0500

    Subiendo vistas

--- restaurant/myapp/views.py
+++ restaurant/myapp/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Bebida
 
-# Create your views here.
+def sobre(request):
+	html = "<h1>Proyecto Django </h1>"
+	return HttpResponse(html)
+
+def lista_bebidas(request):
+	bebidas = Bebida.objects.all()
+	return render(request,'bebidas.html',{'bebidas':bebidas})
