8f1c2668c8f34d6c38ff64e3559708af5c3c23f7
pacifi <pacifi.bnr@gmail.com>
2015-08-23 14:43:27 -0500

    nuevo

--- restaurant/restaurant/settings.py
+++ restaurant/restaurant/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'restaurant.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'restaurantdb',
+        'USER':'restaurantuser',
+        'PASSWORD': '123456',
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
 
