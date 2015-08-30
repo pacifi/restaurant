98f67c731c55ed29983eb3a8228599e7f1d9b028
pacifi <pacifi.bnr@gmail.com>
2015-08-30 12:59:46 -0500

    detalle receta

--- restaurant/myapp/views.py
+++ restaurant/myapp/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from .models import Bebida, Receta
 
@@ -14,3 +14,11 @@ def inicio(request):
 	recetas = Receta.objects.all()
 	total = recetas.count()
 	return render(request, 'recetas/inicio.html',{'recetas':recetas, 'total':total})
+
+def lista_recetas(request):
+	recetas = Receta.objects.all()
+	return render(request, 'recetas/recetas.html',{'recetas':recetas})
+
+def detalle_receta(request, id_receta):
+	receta = get_object_or_404(Receta, id=id_receta)
+	return render(request, 'recetas/receta.html', {'receta':receta})
--- restaurant/restaurant/urls.py
+++ restaurant/restaurant/urls.py
@@ -1,10 +1,14 @@
 
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^sobre/$','myapp.views.sobre'),
     url(r'^bebidas/$','myapp.views.lista_bebidas'),
     url(r'^$','myapp.views.inicio'),
-]
+    url(r'^recetas/$','myapp.views.lista_recetas'),
+    url(r'^receta/(?P<id_receta>\d+)$','myapp.views.detalle_receta'),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
