0db944c61116939d23473a2ba59c105305892366
pacifi <pacifi.bnr@gmail.com>
2015-08-30 11:48:09 -0500

    funcion inicio

--- restaurant/myapp/views.py
+++ restaurant/myapp/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import Bebida
+from .models import Bebida, Receta
 
 def sobre(request):
 	html = "<h1>Proyecto Django </h1>"
@@ -9,3 +9,8 @@ def sobre(request):
 def lista_bebidas(request):
 	bebidas = Bebida.objects.all()
 	return render(request,'bebidas.html',{'bebidas':bebidas})
+
+def inicio(request):
+	recetas = Receta.objects.all()
+	total = recetas.count()
+	return render(request, 'recetas/inicio.html',{'recetas':recetas, 'total':total})
